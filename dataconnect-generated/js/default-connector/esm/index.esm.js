d25943982b8cf6f42b537352713a749d1a5bd429
Thanniru Sai Teja <iamsaitejathanniru@gmail.com>
2025-04-23 23:56:03 +0530

    First Commit

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'chalana-chitram',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,8 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'chalana-chitram',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
+
